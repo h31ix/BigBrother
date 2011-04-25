9c32045ce583bbae8d5cdab95f524d8713fa4c48
Rob "N3X15" Nelson <nexis@7chan.org>
2011-04-25 08:47:42 -0700

    CB 720 breaking shit

--- src/test/java/me/taylorkelly/bigbrother/tests/TestUtils.java
+++ src/test/java/me/taylorkelly/bigbrother/tests/TestUtils.java
@@ -18,51 +18,8 @@ public class TestUtils {
      * @return a 
      */
     public static World createSimpleWorld() {
-        if(world==null) {
-            // CraftBukkit start
-            // TerraBukkit start
-            boolean hell = false;
-            boolean mobs = false;
-            boolean animals = false;
-            
-            String worldname="testworld";
-            long seed=1234;
-            world = new WorldServer(null, new ServerNBTManager(new File("tests"),worldname, true), worldname, hell ? -1 : 0, seed, null, null);
-            // TerraBukkit end
-            world.a(new WorldManager(null, world));
-            world.j = mobs ? 1 : 0;
-            world.a(mobs, animals);
-            // CraftBukkit end
-            
-            ChunkCoordinates chunkcoordinates = world.m(); // CraftBukkit
-
-            short radius = 196;
-            long j = System.currentTimeMillis();
-            for (int k = -radius; k <= radius; k += 16) {
-                for (int l = -radius; l <= radius; l += 16) {
-                    long i1 = System.currentTimeMillis();
-
-                    if (i1 < j) {
-                        j = i1;
-                    }
-
-                    if (i1 > j + 1000L) {
-                        int j1 = (radius * 2 + 1) * (radius * 2 + 1);
-                        int k1 = (k + radius) * (radius * 2 + 1) + l + 1;
-                        BBLogging.debug("Preparing spawn area: "+(k1 * 100 / j1)+"%");
-                        j = i1;
-                    }
-
-                    world.u.c(chunkcoordinates.a + k >> 4, chunkcoordinates.c + l >> 4);
-                    
-                    while (world.f()) {
-                        ;
-                    }
-                    
-                }
-            }
-        }
-        return (World)world.getWorld();
+    	//TODO: Broken
+        return null;
     }
     
 }
