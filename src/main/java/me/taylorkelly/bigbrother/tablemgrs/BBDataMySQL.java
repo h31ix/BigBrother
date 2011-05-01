a781abf0e2f1c4b50b3d2c805e4dbd110f4f22e2
N3X15 <nexis@7chan.org>
2011-05-01 10:50:38 -0700

    HELPS TO INCLUDE DELIMITERS, NEXYPOO

--- src/main/java/me/taylorkelly/bigbrother/tablemgrs/BBDataMySQL.java
+++ src/main/java/me/taylorkelly/bigbrother/tablemgrs/BBDataMySQL.java
@@ -164,8 +164,8 @@ public class BBDataMySQL extends BBDataTable {
 	@Override
 	public String getCleanseByLimit(Long maxRecords, long deletesPerCleansing) {
 		// Fucking MySQL and your lack of subqueries LIMIT support.
-		String cleansql = "CREATE TEMPORARY TABLE top_record SELECT id FROM bbdata ORDER BY id DESC LIMIT "+maxRecords;
-		cleansql+="DELETE FROM bbdata WHERE id NOT IN (SELECT id FROM top_record) LIMIT "+deletesPerCleansing;
+		String cleansql = "CREATE TEMPORARY TABLE top_record SELECT id FROM bbdata ORDER BY id DESC LIMIT "+maxRecords+"; ";
+		cleansql+="DELETE FROM bbdata WHERE id NOT IN (SELECT id FROM top_record) LIMIT "+deletesPerCleansing+"; ";
 		cleansql+="DROP TABLE top_record;";
 
 
