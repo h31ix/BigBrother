b662c1b99a547e0df1f23760f566370979ee02e2
N3X15 <nexis@7chan.org>
2011-06-20 22:44:25 -0700

    LOLDERP

--- src/test/java/me/taylorkelly/bigbrother/tests/TestUtils.java
+++ src/test/java/me/taylorkelly/bigbrother/tests/TestUtils.java
@@ -1,15 +1,5 @@
 package me.taylorkelly.bigbrother.tests;
 
-import java.io.File;
-
-import net.minecraft.server.EntityTracker;
-import net.minecraft.server.SecondaryWorldServer;
-import net.minecraft.server.ServerNBTManager;
-import net.minecraft.server.WorldManager;
-import net.minecraft.server.WorldServer;
-
-import org.bukkit.World;
-import org.bukkit.World.Environment;
 
 public class TestUtils {
     
@@ -18,7 +8,8 @@ public class TestUtils {
     /** 
      * Initialize and populate a simple world. USED FOR JUnit TESTS ONLY
      * @return a 
-     *//*
+     */
+    /*
     public static World createSimpleWorld() {
         world = new WorldServer(null, new ServerNBTManager(new File("world"), "world", true), "world", 0, 3, Environment.NORMAL, null, null); // CraftBukkit
         world.spawnMonsters = 0;
