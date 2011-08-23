afa1459f9d6d1693b8c18c0fc88e9d9ea00ccf88
N3X15 <nexis@7chan.org>
2011-08-23 16:38:49 -0700

    Take a stab at fixing config tests

--- src/test/java/me/taylorkelly/bigbrother/tests/TestBigBrother.java
+++ src/test/java/me/taylorkelly/bigbrother/tests/TestBigBrother.java
@@ -1,6 +1,7 @@
 package me.taylorkelly.bigbrother.tests;
 
 import java.io.File;
+import java.sql.SQLException;
 
 import me.taylorkelly.bigbrother.BBSettings;
 import me.taylorkelly.bigbrother.BBSettings.DBMS;
@@ -8,7 +9,9 @@ import me.taylorkelly.bigbrother.datasource.BBDB;
 import me.taylorkelly.bigbrother.tablemgrs.ActionTable;
 
 import org.bukkit.util.config.Configuration;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestBigBrother {
     private File testFolder;
@@ -28,7 +31,7 @@ public class TestBigBrother {
     }
     
     @Test
-    public void configLoading() {
+    public void configLoading() throws SQLException {
         File dataFolder = new File(testFolder, "configLoading");
         dataFolder.mkdirs();
         File settingsFile = new File(dataFolder, "BigBrother.yml");
@@ -38,6 +41,10 @@ public class TestBigBrother {
         cfg.save();
         
         BBSettings.initialize(null, dataFolder);
+        
+        // Get database running (ActionTable init requires it)
+        BBDB.reconnect(); // Do NOT catch.
+        
         ActionTable.getInstance().init();
         BBSettings.loadPostponed();
         Assert.assertTrue("Configuration didn't get saved.", BBDB.usingDBMS(DBMS.MYSQL));
