425dfaf7a447d5e4025c96689bb8f658bb85b6b0
Spencer Lowe <gkwr@iup.edu>
2011-08-24 23:45:47 -0400

    Updated Imports

--- src/main/java/me/taylorkelly/bigbrother/commands/UnwatchedCommand.java
+++ src/main/java/me/taylorkelly/bigbrother/commands/UnwatchedCommand.java
@@ -4,6 +4,7 @@ import me.taylorkelly.bigbrother.BBPermissions;
 import me.taylorkelly.bigbrother.BigBrother;
 
 import org.bukkit.command.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
--- src/main/java/me/taylorkelly/bigbrother/commands/WatchedCommand.java
+++ src/main/java/me/taylorkelly/bigbrother/commands/WatchedCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 public class WatchedCommand implements CommandExecutor {
     private BigBrother plugin;
